7bce100d38f6a90554b31bec6c8df985c8eb0245
Ben <Datan@users.noreply.github.com>
2015-02-27 16:19:59 -0800

    Book.cpp file
    Implementation file for Book class.

---
+++ Project/Book.cpp
@@ -0,0 +1,67 @@
+#include "Book.h"
+//#include "Name.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+Book::Book()
+{
+	//MUST FILL IN
+	//
+	//
+	//
+}
+
+Book::Book(int eISBN, string eTitle, Name eAuthor, string ePublisher)
+{
+	ISBN = eISBN;
+	title = eTitle;
+	author = eAuthor;
+	publisher = ePublisher;
+}
+
+void Book::setISBN(int eISBN)
+{
+	while (eISBN < 0)
+	{
+		cout << "Please enter a non-negative number." << endl;
+		cin >> eISBN;
+	}
+	ISBN = eISBN;
+}
+
+void Book::setTitle(string eTitle)
+{
+	title = eTitle;
+}
+
+void Book::setAuthor(Name eAuthor)
+{
+	author = eAuthor;
+}
+
+void Book::setPublisher(string ePublisher)
+{
+	publisher = ePublisher;
+}
+
+int Book::getISBN()
+{
+	return ISBN;
+}
+
+string Book::getTitle()
+{
+	return title;
+}
+
+string Book::getAuthor()
+{
+	return author.GetName();
+}
+
+string Book::getPublisher()
+{
+	return publisher;
+}
