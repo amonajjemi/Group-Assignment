5b87d1d218bd1ae87c8f270bd29486e3adf7667d
Ben <benjamin.j.duggan@gmail.com>
2015-02-28 13:25:09 -0800

    Database.h and Database.cpp - Moved database functions to their own files

---
+++ Project/Database.h
@@ -0,0 +1,13 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include "InventoryItem.h"
+
+
+char Choice(char, char);
+char YesNo();
+void Database();
+void DatabaseMenu(InventoryItem*, int);
+
+
+#endif DATABASE_H
