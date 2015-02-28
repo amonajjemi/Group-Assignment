c3d4c0c835e30b5f40e4ff6e07cc9f7f15def63e
Ben <Datan@users.noreply.github.com>
2015-02-27 16:17:28 -0800

    Updated Date.cpp file
    Added operator overloading for relational operators and assignment operator
    (>, >=, <, <=, ==, and =)

--- Project/Date.cpp
+++ Project/Date.cpp
@@ -8,15 +8,17 @@ Date::Date(){
 	Day = 1;
 	Month = 1;
 	Year = 1900;
+	
 }
 Date::Date(const Date &obj){
 	Day = obj.Day;
 	Month = obj.Month;
 	Year = obj.Year;
+	
 }
 Date::Date(int month, int day, int year){
 	if (month > 12)
-		month = 12;
+		 month = 12;
 	switch (month)
 	{
 	case 1:
@@ -71,9 +73,110 @@ Date::Date(int month, int day, int year){
 	Day = day;
 	Month = month;
 	Year = year;
+	
 }
 Date::~Date(){
-
+	
+		
+}
+bool Date::operator>(const Date &obj){
+	if (Year > obj.Year)
+		return true;
+	else if (Year < obj.Year)
+		return false;
+	else
+	{
+		if (Month > obj.Month)
+			return true;
+		else if (Month < obj.Month)
+			return false;
+		else
+		{
+			if (Day > obj.Day)
+				return true;
+			else if (Day < obj.Day)
+				return false;
+			else
+				return false;
+		}
+	}
+}
+bool Date::operator>=(const Date &obj){
+	if (Year > obj.Year)
+		return true;
+	else if (Year < obj.Year)
+		return false;
+	else
+	{
+		if (Month > obj.Month)
+			return true;
+		else if (Month < obj.Month)
+			return false;
+		else
+		{
+			if (Day > obj.Day)
+				return true;
+			else if (Day < obj.Day)
+				return false;
+			else
+				return true;
+		}
+	}
+}
+bool Date::operator<(const Date &obj){
+	if (Year > obj.Year)
+		return false;
+	else if (Year < obj.Year)
+		return true;
+	else
+	{
+		if (Month > obj.Month)
+			return false;
+		else if (Month < obj.Month)
+			return true;
+		else
+		{
+			if (Day > obj.Day)
+				return false;
+			else if (Day < obj.Day)
+				return true;
+			else
+				return false;
+		}
+	}
+}
+bool Date::operator<=(const Date &obj){
+	if (Year > obj.Year)
+		return false;
+	else if (Year < obj.Year)
+		return true;
+	else
+	{
+		if (Month > obj.Month)
+			return false;
+		else if (Month < obj.Month)
+			return true;
+		else
+		{
+			if (Day > obj.Day)
+				return false;
+			else if (Day < obj.Day)
+				return true;
+			else
+				return true;
+		}
+	}
+}
+bool Date::operator==(const Date &obj){
+	if (Year == obj.Year && Month == obj.Month && Day == obj.Day)
+		return true;
+	else
+		return false;		
+}
+void Date::operator=(const Date &obj){
+	Day = obj.Day;
+	Month = obj.Month;
+	Year = obj.Year;
 }
 //Setters
 void Date::SetDay(int day){ Day = day; }
@@ -87,12 +190,13 @@ string Date::GetDate(){// Returns the date object as a string, in the format of 
 	string str;
 	str = to_string(Year) + "/";
 	if (to_string(Month).length() == 1)
-		str += "0" + to_string(Month) + "/";
+		 str += "0" + to_string(Month) + "/";
 	else
-		str += to_string(Month) + "/";
+		 str += to_string(Month) + "/";
 	if (to_string(Day).length() == 1)
-		str += "0" + to_string(Day);
+		 str += "0" + to_string(Day);
 	else
-		str += to_string(Day);
+		 str += to_string(Day);
 	return str;
+	
 }
