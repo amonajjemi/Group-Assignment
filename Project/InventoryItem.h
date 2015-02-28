44c208ef42ad96ce55ef8555b9058d5dd525f5ed
Ben <Datan@users.noreply.github.com>
2015-02-27 16:20:31 -0800

    InventoryItem.h file
    Header file for InventoryItem class

---
+++ Project/InventoryItem.h
@@ -0,0 +1,32 @@
+#ifndef INVENTORYITEM_H
+#define INVENTORYITEM_H
+#include "Book.h"
+#include "Date.h"
+
+class InventoryItem
+{
+private:
+	Book BookItem;
+	Date DateAdded;
+	int Quantity;
+	double Wholesale;
+	double Price;
+public:
+	InventoryItem();
+	InventoryItem(Book, Date, int, double, double);
+	~InventoryItem();
+
+	void setBook(Book);
+	void setDateAdded(Date);
+	void setQuantity(int);
+	void setWholesale(double);
+	void setPrice(double);
+
+	Book getBook();
+	string getDateAdded();
+	int getQuantity();
+	double getWholesale();
+	double getPrice();
+};
+
+#endif
