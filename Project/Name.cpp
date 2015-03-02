eafb31061ba7e5b61f028aed03240bb755245031
Ben <benjamin.j.duggan@gmail.com>
2015-03-02 10:38:51 -0800

    Name.cpp and Name.h updates - Added io stream overload

--- Project/Name.cpp
+++ Project/Name.cpp
@@ -3,20 +3,27 @@
 
 using namespace std;
 
-Name::Name(){
-	First = "";
+Name::Name(){ 
 	Last = "";
+	First = "";
 }
 Name::Name(string last, string first){
-	First = first;
 	Last = last;
+	First = first;
 }
 Name::Name(const Name &obj){
-	First = obj.First;
 	Last = obj.Last;
+	First = obj.First;
 }
 Name::~Name(){
 }
+
+void Name::SetFirstName(string first){ First = first; }
+void Name::SetLastName(string last){ Last = last; }
+string Name::GetFirstName(){ return First; }
+string Name::GetLastName(){ return Last; }
+string Name::GetstrName(){ return Last + ", " + First; }
+
 bool Name::operator>(const Name &obj){
 	// Greater than operator for the Name class
 	// Compares the left hand side to the right hand side (obj is right hand side)
@@ -67,11 +74,21 @@ void Name::operator=(const Name &obj){
 	First = obj.First;
 	Last = obj.Last;
 }
-void Name::SetFirstName(string first){ First = first; }
-void Name::SetLastName(string last){ Last = last; }
-string Name::GetFirstName(){ return First; }
-string Name::GetLastName(){ return Last; }
-string Name::GetName(){ return Last + ", " + First; }
-
-
-//TEST
+ostream& operator<<(ostream &os, const Name &obj){
+	os << obj.Last << ", " << obj.First;
+	return os;
+}
+istream& operator>>(istream &is, Name &obj){
+	is >> obj.Last >> obj.First;
+	return is;
+}
+/*
+ofstream& operator<<(ofstream &ofs, const Name &obj){
+	ofs << obj.Last << " " << obj.First;
+	return ofs;
+}
+ifstream& operator>>(ifstream &ifs, Name &obj){
+	ifs >> obj.Last >> obj.First;
+	return ifs;
+}
+*/
